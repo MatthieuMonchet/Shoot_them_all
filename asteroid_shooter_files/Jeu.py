c505712365c5f115712877f7cf0e2996cbfd5d82
Matthieu Monchet <matthieu.monchet@gmail.com>
2022-07-09 21:00:00 +0200

    feat rectt

--- asteroid_shooter_files/Jeu.py
+++ asteroid_shooter_files/Jeu.py
@@ -4,8 +4,10 @@ pygame.init()
 WINDOW_WIDTH,WINDOW_HEIGHT = 1280,720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("Jeu de Vaisseaux")
+clock = pygame.time.Clock()
 # import des images
 ship_surface = pygame.image.load(r"../Jeu de vaisseaux/asteroid_shooter_files/project_2 - Surfaces/graphics/ship.png").convert_alpha()
+ship_rect = ship_surface.get_rect(center = (WINDOW_WIDTH/2,WINDOW_HEIGHT/2))
 # convert_alpha quand le sprite ne couvre pas toute la surface
 background_surface = pygame.image.load(r"../Jeu de vaisseaux/asteroid_shooter_files/project_2 - Surfaces/graphics/background.png").convert() 
 # import de la police
@@ -18,10 +20,11 @@ while True: # garde le jeu ouvert
         if event.type == pygame.QUIT: # clic sur la croix de la fenetre de jeu
             pygame.quit()
             sys.exit() # pour sortir du code sans ça erreur car la boucle continue
+    clock.tick(60) # On bride le framerate
     # 2. updates
     display_surface.fill((0,0,0))
     display_surface.blit(background_surface,(0,0))
-    display_surface.blit(ship_surface,(50,500))
+    display_surface.blit(ship_surface,ship_rect)
     display_surface.blit(text_font,(500,200))
 
 
