5f40342b9673d21f41bbcc160b482d109b408928
Matthieu Monchet <matthieu.monchet@gmail.com>
2022-07-09 20:18:26 +0200

    feat text

--- asteroid_shooter_files/Jeu.py
+++ asteroid_shooter_files/Jeu.py
@@ -1,4 +1,3 @@
-from turtle import window_height, window_width
 import pygame, sys
 
 pygame.init()
@@ -9,18 +8,22 @@ pygame.display.set_caption("Jeu de Vaisseaux")
 ship_surface = pygame.image.load(r"../Jeu de vaisseaux/asteroid_shooter_files/project_2 - Surfaces/graphics/ship.png").convert_alpha()
 # convert_alpha quand le sprite ne couvre pas toute la surface
 background_surface = pygame.image.load(r"../Jeu de vaisseaux/asteroid_shooter_files/project_2 - Surfaces/graphics/background.png").convert() 
-
+# import de la police
+font = pygame.font.Font(r"../Jeu de vaisseaux/asteroid_shooter_files/project_2 - Surfaces/graphics/subatomic.ttf",50)
+text_font = font.render("SPACE",True,(255,255,255))
 while True: # garde le jeu ouvert
 
     # 1. inputs (click, mouse movement, etc) --> events dans pygame
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # cliq sur la croix de la fenetre de jeu
+        if event.type == pygame.QUIT: # clic sur la croix de la fenetre de jeu
             pygame.quit()
             sys.exit() # pour sortir du code sans ça erreur car la boucle continue
     # 2. updates
     display_surface.fill((0,0,0))
     display_surface.blit(background_surface,(0,0))
     display_surface.blit(ship_surface,(50,500))
+    display_surface.blit(text_font,(500,200))
+
 
     
     # 3. afficher l'image
